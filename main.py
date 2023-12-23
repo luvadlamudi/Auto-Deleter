f0397a6c348b3e28947f194dc7dce52d96207dbe
lucas <lucasvad123@gmail.com>
2023-12-23 12:28:41 -0600

    end 1

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-a
+
+import os
+import time
+import calendar
+import shutil
+#str(int(calendar.timegm(time.gmtime())))
+# main function
+def main():
+
+	
+
+
+	# specify the path
+	path = r"C:\Users\elena\OneDrive\Desktop\testFiles"
+
+	
+
+	# checking whether the file is present in path or not
+	if os.path.exists(path):
+
+		for file in path:
+			if "-ocr" in file:
+				
+
+
+
+
