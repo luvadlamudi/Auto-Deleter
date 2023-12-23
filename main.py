899a0d2447c3b276a89914f787a51954b8861157
lucas <lucasvad123@gmail.com>
2023-12-23 12:36:07 -0600

    integrated delete

--- main.py
+++ main.py
@@ -7,21 +7,32 @@ import shutil
 # main function
 def main():
 
-	
-
-
 	# specify the path
 	path = r"C:\Users\elena\OneDrive\Desktop\testFiles"
-
 	
-
 	# checking whether the file is present in path or not
 	if os.path.exists(path):
-
-		for file in path:
+    
+		for file in os.walk(path):
+			
 			if "-ocr" in file:
-				
+				dirname = os.path.dirname(path)
+				file_path = os.path.join(dirname, file)
 
+				remove_file(file_path)
 
+def remove_file(path):
 
+	# removing the file
+	if not os.remove(path):
 
+		# success message
+		print(f"{path} is removed successfully")
+
+	else:
+
+		# failure message
+		print(f"Unable to delete the {path}")
+
+if __name__ == '__main__':
+	main()
