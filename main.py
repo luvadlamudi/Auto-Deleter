35d55753c07c397bf8515d6c222d160070138e56
lucas <lucasvad123@gmail.com>
2023-12-23 12:48:36 -0600

    finished

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-
+#import libraries
 import os
-#import time
-#import calendar
-import shutil
-#str(int(calendar.timegm(time.gmtime())))
+
 # main function
 def main():
 
-	# specify the path
-	path = r"C:\Users\elena\OneDrive\Desktop\testFiles"
-	
-	# check if there are files in the selected path
-	if os.path.exists(path):
-		for file in os.walk(path):
-			
-			if "-ocr" in file:
-				os.remove(file)
+  # specify the path
+  path = r"C:\Users\elena\OneDrive\Desktop\testFiles"
 
+  # check if there are files in the selected path
+  if os.path.exists(path):
+      for root, dirs, files in os.walk(path):
+          for file in files:
+              if "-ocr" in file:
+                  os.remove(os.path.join(root, file))
+
+if __name__ == "__main__":
+    main()
