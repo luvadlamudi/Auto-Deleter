d647f26ba627d6a0d3f8ebee4ef4630143e6685d
lucas <lucasvad123@gmail.com>
2023-12-23 12:39:27 -0600

    r2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import os
-import time
-import calendar
+#import time
+#import calendar
 import shutil
 #str(int(calendar.timegm(time.gmtime())))
 # main function
@@ -10,29 +10,10 @@ def main():
 	# specify the path
 	path = r"C:\Users\elena\OneDrive\Desktop\testFiles"
 	
-	# checking whether the file is present in path or not
+	# check if there are files in the selected path
 	if os.path.exists(path):
-    
 		for file in os.walk(path):
 			
 			if "-ocr" in file:
-				dirname = os.path.dirname(path)
-				file_path = os.path.join(dirname, file)
+				os.remove(file)
 
-				remove_file(file_path)
-
-def remove_file(path):
-
-	# removing the file
-	if not os.remove(path):
-
-		# success message
-		print(f"{path} is removed successfully")
-
-	else:
-
-		# failure message
-		print(f"Unable to delete the {path}")
-
-if __name__ == '__main__':
-	main()
